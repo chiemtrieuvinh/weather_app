52165aab108ca6ecfea475607500e415784e2c0c
trieuvinb@gmail.com <trieuvinb@gmail.com>
2021-06-06 20:32:55 +0700

    [update] create folders and configs

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Home from 'components/home/Home';
+import logo from './logo.svg';
+
+import './App.css';
+
+const App: React.FC<{}> = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>
+
+        Hello world !
+
+      </h1>
+
+      <p>
+
+        Home component
+
+      </p>
+    </header>
+  </div>
+);
+
+export default App;
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home: React.FC<{}> = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>
+        Hello world !
+      </h1>
+      <p>
+        Home component
+      </p>
+    </header>
+  </div>
+);
+
+export default Home;
