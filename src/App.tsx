24d541de2e59907885aeb1433c4d23aa88ec5ada
trieuvinb@gmail.com <trieuvinb@gmail.com>
2021-06-14 11:49:35 +0700

    [update] file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import Home from 'components/Home/main';
-import LayoutWrapper from 'components/LayoutWrapper/main';
+import Home from 'components/pages/Home/main';
+import LayoutWrapper from 'components/organismos/LayoutWrapper/main';
 import 'styles/main.scss';
 import './App.css';
+import mock from './mock.json';
+import weather from './weather.json';
 
-const App: React.FC<{}> = () => (
-  <div className="App">
-    <header />
-    <body>
-      <LayoutWrapper>
-        <Home />
-      </LayoutWrapper>
-    </body>
-  </div>
-);
+const App: React.FC<{}> = () => {
+  console.log(mock, '???');
+  console.log(weather, 'weather');
+  return (
+    <div className="App">
+      <header />
+      <body>
+        <LayoutWrapper>
+          <Home />
+        </LayoutWrapper>
+      </body>
+    </div>
+  );
+};
 
 export default App;
