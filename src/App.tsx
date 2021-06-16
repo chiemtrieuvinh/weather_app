4f64df4f8e6871ae1701574c60cc545f996efc61
trieuvinb@gmail.com <trieuvinb@gmail.com>
2021-06-16 23:43:36 +0700

    clean filles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import Home from 'components/pages/Home/main';
 import LayoutWrapper from 'components/organismos/LayoutWrapper/main';
+import Image from 'components/atoms/Image/main';
 import 'styles/main.scss';
 import './App.css';
-import mock from './mock.json';
-import weather from './weather.json';
 
-const App: React.FC<{}> = () => {
-  console.log(mock, '???');
-  console.log(weather, 'weather');
-  return (
-    <div className="App">
-      <header />
-      <body>
-        <LayoutWrapper>
-          <Home />
-        </LayoutWrapper>
-      </body>
-    </div>
-  );
-};
+const App: React.FC<{}> = () => (
+  <div className="App">
+    <header />
+    <body>
+      <LayoutWrapper>
+        <Image src="https://wallpapercave.com/wp/wp6175133.jpg" alt="background-image" styleProps={{ position: 'absolute' }} />
+        <div className="BlackLayer" />
+        <Home />
+      </LayoutWrapper>
+    </body>
+  </div>
+);
 
 export default App;
