ade88682b2c8a381ad761db4b755f7000f5d5604
trieuvinb@gmail.com <trieuvinb@gmail.com>
2021-07-12 01:18:00 +0700

    [Update] change Home main layout

--- src/components/pages/Home/main.tsx
+++ src/components/pages/Home/main.tsx
@@ -6,6 +6,10 @@ import FlexBox from 'components/atoms/FlexBox/main';
 import FlexBoxItem from 'components/atoms/FlexBoxItem/main';
 import { ConsolidatedWeather } from 'types/WeatherLocationDayType';
 import Text from 'components/atoms/Text/main';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 import UseMetaWeather from './hooks';
 
 const Home: React.FC<{ styleProps?: StyleProps }> = ({ styleProps = {} }) => {
@@ -17,37 +21,46 @@ const Home: React.FC<{ styleProps?: StyleProps }> = ({ styleProps = {} }) => {
   };
 
   return (
-    <div className="Home" style={{ ...styleProps }}>
-      <div className="ContentWrapper">
-        <div className="WhiteLayer" />
-        <div className="ContentItem">
-          <div>
-            <h1>5 DAY FORECAST</h1>
-            <SearchInput id="home_search_input" name="searchValue" value={searchValue} onChange={handleChange} styleProps={{ marginTop: 20 }} />
-            <FlexBox styleProps={{ marginTop: 20 }}>
-              {
-                metaWeatherState && metaWeatherState.consolidated_weather && metaWeatherState.consolidated_weather.map((item: ConsolidatedWeather) => (
-                  <FlexBoxItem>
-                    <WeatherCard
-                      day={item.applicable_date}
-                      weatherState={item.weather_state_abbr}
-                      weatherStateName={item.weather_state_name}
-                      minTemp={Math.round(item.min_temp)}
-                      maxTemp={Math.round(item.max_temp)}
-                    />
-                  </FlexBoxItem>
-                ))
-              }
-            </FlexBox>
-            <FlexBox styleProps={{ marginTop: 20 }}>
-              <Text size={36} styleProps={{ width: '100%', textAlign: 'center' }}>
-                {`${metaWeatherState && metaWeatherState.parent && metaWeatherState.parent.title} - ${metaWeatherState && metaWeatherState.title}`}
-              </Text>
-            </FlexBox>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Container className="h-100">
+      <Row className="row mx-auto">
+        <Row className="row mx-auto">
+          <Col className="text-center">
+            <Text className="xl-text otomano-font" color="white" styleProps={{ letterSpacing: 4, fontWeight: 500, marginBottom: 20 }}>5 DAY FORECAST</Text>
+          </Col>
+        </Row>
+        <Row className="row mx-auto">
+          <Col>
+            <SearchInput id="searchInput" name="searchValue" value={searchValue} onChange={handleChange} />
+          </Col>
+        </Row>
+      </Row>
+      <Row className="row mx-auto d-flex align-items-center justify-content-center" style={{ height: '40%' }}>
+        <Col>
+          <Text className="lg-text otomano-font text-center" color="white" styleProps={{ letterSpacing: 4, fontWeight: 500, marginBottom: 20 }}>
+            Location:
+            {' '}
+            {metaWeatherState?.parent?.title}
+          </Text>
+        </Col>
+      </Row>
+      <Row className="row mx-auto w-95 pb-2 mt-1">
+        {
+          metaWeatherState && metaWeatherState.consolidated_weather && metaWeatherState.consolidated_weather.map((item: ConsolidatedWeather) => (
+            <Col className="d-flex align-items-end justify-content-center w-95 pb-sm-1">
+              <div className="p-4 shadowEffect">
+                <WeatherCard
+                  day={item.applicable_date}
+                  weatherState={item.weather_state_abbr}
+                  weatherStateName={item.weather_state_name}
+                  minTemp={Math.round(item.min_temp)}
+                  maxTemp={Math.round(item.max_temp)}
+                />
+              </div>
+            </Col>
+          ))
+        }
+      </Row>
+    </Container>
   );
 };
 
